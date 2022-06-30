397446e70a36c01256dcec7a360f07fcb64ccfac
Jon Friend <62109917+jonnyfriendlychicago@users.noreply.github.com>
2022-06-30 13:11:37 -0500

    first commit

---
+++ src/main/java/com/jonfriend/java22displaydate/Java22displaydateApplication.java
@@ -0,0 +1,13 @@
+package com.jonfriend.java22displaydate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java22displaydateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java22displaydateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonfriend/java22displaydate/Java22displaydateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonfriend.java22displaydate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java22displaydateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
